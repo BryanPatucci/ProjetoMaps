6187e18ffb30e77ac37cb97dbf9dd5e89e98189c
BryanPatucci <bryanpatuccisilva2002@gmail.com>
2019-09-19 04:07:04 +0900

    Foi finalizado o projeto colocando o mapa, o estilo e muitas outras coisas.

--- www/components/sistema.js
+++ www/components/sistema.js
@@ -1,11 +1,10 @@
 $(document).on("click", "#botao", function(){
   L.mapquest.key = 'afBbkH6L7Q1vhevaj5NOL9vWipxf8tCR';
 
-  // 'map' refers to a <div> element with the ID map
   L.mapquest.map('map', {
-    center: [37.7749, -122.4194],
+    center: [-24.1226656,-46.6784699],
     layers: L.mapquest.tileLayer('map'),
-    zoom: 12
+    zoom:17
   });
 });
 
